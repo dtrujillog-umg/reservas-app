6f0e8649a1e338ee71f480fa2bbd16f9718c9f22
Diego Trujillo <72467997+dtrujillog-umg@users.noreply.github.com>
2023-06-06 11:50:05 -0600

    0606202
    Creacion del frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: '', component: MenuComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
